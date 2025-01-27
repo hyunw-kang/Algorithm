f959b4c7904c66d1da0e1e886c607dcd08225a17
hyunw-kang <156151246+hyunw-kang@users.noreply.github.com>
2025-01-27 17:38:31 +0900

    [level 2] Title: 올바른 괄호, Time: 19.60 ms, Memory: 54.3 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -1,22 +1,23 @@
+import java.util.*;
 class Solution {
-
     boolean solution(String s) {
-        int openCount = 0;
-        int closeCount = 0;
+        boolean answer = true;
+        Stack<Character> stack = new Stack<>();
+        
+        for(char item : s.toCharArray()){
+            if(item == '('){
+                stack.push('(');
+            }
+            else{
+                if(!stack.isEmpty()){
+                    stack.pop();
+                }
+                else {
+                    return false;
+                }
+            }
+        }
 
-        for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) == '(') {
-                openCount++;
-            } else if (s.charAt(i) == ')') {
-                closeCount++;
-            }
-            if (openCount < closeCount) {
-                return false;
-            }
-        }
-        if (openCount == closeCount) {
-            return true;
-        }
-        return false;
+        return stack.isEmpty()?true : false;
     }
 }
