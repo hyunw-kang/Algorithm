72a47220d43fa2ca3ac1165871a937b64d3c7061
hyunw-kang <156151246+hyunw-kang@users.noreply.github.com>
2025-09-15 15:34:40 +0900

    [Silver II] Title: 쇠막대기, Time: 152 ms, Memory: 16352 KB -BaekjoonHub

---
+++ 백준/Silver/10799. 쇠막대기/쇠막대기.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+import java.util.Stack;
+import java.util.*;
+
+public class Main{
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input = br.readLine();
+        Stack<Character> stack = new Stack<>();
+        int result = 0;
+
+        for(int i = 0; i < input.length(); i++){
+            char c = input.charAt(i);
+            if(c == '('){
+                stack.push(c);
+            } else{
+                stack.pop();
+                if(input.charAt(i-1) == '('){
+                    result += stack.size();
+                } else{
+                    result += 1;
+                }
+            }
+        }
+        System.out.println(result);
+    }
+}
