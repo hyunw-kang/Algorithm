e0000179cf43ad986349eae0ae818e43a7b998b1
hyunw-kang <156151246+hyunw-kang@users.noreply.github.com>
2025-09-23 17:09:14 +0900

    [Gold IV] Title: 회전 초밥, Time: 492 ms, Memory: 170964 KB -BaekjoonHub

--- 백준/Gold/15961. 회전 초밥/회전 초밥.java
+++ 백준/Gold/15961. 회전 초밥/회전 초밥.java
@@ -1,43 +1,56 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
-public class Main {
+public class Main{
+    static int N,d,k,c;
+    static int[] sushi, visited;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer tk = new StringTokenizer(br.readLine());
 
-    public static  void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        int d = sc.nextInt();
-        int k = sc.nextInt();
-        int c = sc.nextInt();
+        N = Integer.parseInt(tk.nextToken());
+        d = Integer.parseInt(tk.nextToken());
+        k = Integer.parseInt(tk.nextToken());
+        c = Integer.parseInt(tk.nextToken());
+        sushi = new int[N];
+        visited = new int[d+1];
 
-        int[] arr = new int[N];
-
-        for (int i = 0; i < N; i++) {
-            arr[i] = sc.nextInt();
+        for(int i=0; i<N; i++) {
+            sushi[i] = Integer.parseInt(br.readLine());
         }
-        int[] eat = new int[d+1];
-        int res = 1;
-        eat[c]++;
+        int total = 0, max = 0;
+        // k개의 window를 처리
 
-        for(int i = 0 ; i < k ; i++) {
-            if(eat[arr[i]]==0){ //처음먹은것이면 res증가
-                res++;
+        for(int i = 0; i < k; i++){
+            int idx = sushi[i];
+            if(visited[idx] == 0){  //먹은 초밥이 아니면 total++
+                total++;
             }
-            eat[arr[i]]++;
+            visited[sushi[i]]++;
         }
+        max = total;
 
-        int cnt = res;
-
-        for(int i = 1 ; i < N; i++) {
-            int pop = arr[i-1];
-            eat[pop]--; //하나 뺌
-            if(eat[pop]==0) cnt--; //종류가 없으면 count 감소
-
-            int add = arr[(i+k-1)%N]; //회전초밥이라 n-k가 아님
-            if(eat[add]==0) cnt++;
-            eat[add]++;
-
-            res = Math.max(res,cnt);
+        // 쿠폰 처리
+        if(visited[c] == 0) {
+            max++;
         }
-        System.out.println(res);
+        // 회전초밥이라 n-k개가 아니라 n개만큼 반복
+        for(int i = 0; i < N; i++){
+            visited[sushi[i]]--;
+            if(visited[sushi[i]] == 0){ // 안먹어본건 --
+                total--;
+            }
+            // 윈도우의 start분 빼고 window의 end분 새로 더하기
+            int index = (i + k) % N; // 회전초밥이라 %N
+            if(visited[sushi[index]] == 0){
+                total++;
+            }
+            visited[sushi[index]]++;
+            // max값 갱신
+            max = Math.max(max, visited[c] == 0 ? total + 1 : total);
+        }
+        System.out.println(max);
     }
 }
