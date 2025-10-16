7bb9432c79249e8a4bab4e0a5732a8ade8ff779f
hyunw-kang <156151246+hyunw-kang@users.noreply.github.com>
2025-10-17 00:37:49 +0900

    [Silver I] Title: 미로 탐색, Time: 212 ms, Memory: 19060 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -1,56 +1,53 @@
-import java.awt.Point;
-import java.util.*;
+import java.awt.*;
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
 
-public class Main{
+public class Main {
+    static boolean visited[][];
+    static int[] dx = {-1,1,0,0};
+    static int[] dy = {0,0,-1,1};
     static int a, b;
-    static int[][] arr;
-    static boolean[][] visited;
-    static int[] dx = {-1, 1, 0, 0};
-    static int[] dy = {0, 0, -1, 1};
+    static int arr[][];
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Scanner sc = new Scanner(System.in);
         a = sc.nextInt();
         b = sc.nextInt();
         arr = new int[a][b];
         visited = new boolean[a][b];
         sc.nextLine();
-
-        for (int i = 0; i < a; i++) {
-            String line = sc.next();
-            for (int j = 0; j < b; j++) {
-                arr[i][j] = line.charAt(j) - '0';
+        for(int i = 0; i< a; i++){
+            String str = sc.next();
+            for(int j = 0; j < b; j++){
+                arr[i][j] = str.charAt(j) - '0';
             }
         }
-
-        bfs(0, 0);   // 시작위치
-
-        System.out.println(arr[a - 1][b - 1]);
-
+        bfs(0,0);
+        System.out.println(arr[a-1][b-1]);
     }
-
-    static void bfs(int x, int y) {
-
-        Queue<Point> queue = new LinkedList<>();
-        queue.offer(new Point(x, y));
+    public static void bfs(int x, int y) {
+        Queue<int[]> q = new LinkedList<>();
+        q.offer(new int[] {x,y});
+        arr[x][y] = 1;
         visited[x][y] = true;
 
-        while (!queue.isEmpty()) {
-            Point currentPoint = queue.poll();
-            for (int i = 0; i < 4; i++) {
-                int nextX = currentPoint.x + dx[i];
-                int nextY = currentPoint.y + dy[i];
-
-                if (nextX < 0 || nextX >= a || nextY < 0 || nextY >= b)
+        while(!q.isEmpty()){
+            int[] current = q.poll();
+            int currentx = current[0];
+            int currenty = current[1];
+            for(int i = 0; i < 4; i++){
+                int nextx = currentx + dx[i];
+                int nexty = currenty + dy[i];
+                if(nextx < 0 || nexty < 0 || nextx >= a || nexty >= b){
                     continue;
-                if (arr[nextX][nextY] == 0)
-                    continue;
-                if (visited[nextX][nextY])
-                    continue;
-                queue.offer(new Point(nextX, nextY));
-                visited[nextX][nextY] = true;
-
-                arr[nextX][nextY] = arr[currentPoint.x][currentPoint.y] + 1;
+                }
+                if(arr[nextx][nexty] == 1 && !visited[nextx][nexty]){
+                    q.offer(new int[] {nextx,nexty});
+                    visited[nextx][nexty] = true;
+                    arr[nextx][nexty] = arr[currentx][currenty] + 1;
+                }
             }
         }
     }
