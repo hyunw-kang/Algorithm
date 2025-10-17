f5165d82f21f2db16fed220eb7ff5731ba2406ff
hyunw-kang <156151246+hyunw-kang@users.noreply.github.com>
2025-10-17 22:23:28 +0900

    [Silver V] Title: 단어 정렬, Time: 564 ms, Memory: 61232 KB -BaekjoonHub

---
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -0,0 +1,34 @@
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        String[] arr = new String[N];
+
+        sc.nextLine();
+
+        for(int i = 0; i < N; i++) {
+            arr[i] = sc.nextLine();
+        }
+        Arrays.sort(arr, new Comparator<String>() {
+            public int compare(String s1, String s2) {
+                if(s1.length() == s2.length()) {
+                    return s1.compareTo(s2);
+                } else {
+                    return s1.length() - s2.length();
+                }
+            }
+        });
+        StringBuilder sb = new StringBuilder();
+        sb.append(arr[0]).append("\n");
+        for(int i = 1; i < N; i++) {
+            if(!arr[i].equals(arr[i-1])) {
+                sb.append(arr[i]).append("\n");
+            }
+        }
+        System.out.print(sb);
+    }
+}
